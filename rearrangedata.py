1bdf76190c24c7adbdda237093016037ea723e30
Alexander Williamson <awilliamson1@postgrad.curtin.edu.au>
2019-09-25 14:25:10 +0800

    script successfully rearranges the output back into input

--- rearrangedata.py
+++ rearrangedata.py
@@ -5,8 +5,9 @@ import os
 if __name__ == '__main__':
     parser = ap.ArgumentParser()
     parser.add_argument("directory")
-    parser.add_argument("nsamples")
-    parser.add_argument("ntiles")
+    parser.add_argument("nsamples", type=np.int)
+    parser.add_argument("ntiles", type=np.int)
+    parser.add_argument("output_file")
 
     args = parser.parse_args()
 
@@ -16,6 +17,10 @@ if __name__ == '__main__':
 
     for tile in range(args.ntiles):
         for pol in range(2):
-            data[:, tile, pol, :, :] = np.fromfile("out_{}_{}.dat", dtype=np.int8).reshape(args.nsamples//51200, 51200, 2)
+            data[:, tile, pol, :, :] = np.fromfile("out_{}_{}.dat".format(tile, pol), dtype=np.int8).reshape(args.nsamples//51200, 51200, 2)
 
-    np.tofile(data)
+    header = np.zeros(4096+102400*2*args.ntiles, np.int8)
+    file = open(args.output_file, "w")
+    header.tofile(file)
+    data.tofile(file)
+    file.close()

3fd7a81f32e18d5417375f0424f2f71b7e74046f
Alexander Williamson <awilliamson1@postgrad.curtin.edu.au>
2019-10-10 17:20:45 +0800

    added paramter to srun command

--- overall_logistics.py
+++ overall_logistics.py
@@ -75,6 +75,7 @@ def run_logistics(srun, parfile, pars, vcs):
               "0,{}".format(pars['ntiles']), "-m"]
     if srun:
         logrun[0] = "srun"
+        logrun.insert(1, "--export=all")
         logrun.insert(3, "-N")
         logrun.insert(4, "{}".format(nnodes))
         logrun.insert(5, "-c")

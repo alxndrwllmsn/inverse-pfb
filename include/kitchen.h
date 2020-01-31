dbefbe727d5cbdd64dd02cda6979ab9ebb1554d5
Alexander Williamson <awilliamson1@postgrad.curtin.edu.au>
2020-01-31 13:48:48 +0800

    Added fft_c2r function and rfftconvolve funcion to deal with purely real outputs

--- include/kitchen.h
+++ include/kitchen.h
@@ -15,8 +15,11 @@ void convolve(float data[],int nsamples, float filter[], int flength,
     float outAr[]);
 void fftconvolve(float rdata[], float idata[], int nsamples, float filter[],
     int flength,float outAr[], float ioutAr[], fftw_plan p, fftw_plan q);
+void rfftconvolve(float rdata[], int nsamples, float filter[], int flength,
+    float outAr[], fftw_plan p, fftw_plan q);
 void fft(float rdata[], float idata[], int nsamples, float odata[],
     float oidata[], const fftw_plan p);
+void fft(float rdata[], int nsamples, float odata[], const fftw_plan p);
 struct dsampled find_downsampled(int fchan, int chanloc, int nchans);
 
 #endif
